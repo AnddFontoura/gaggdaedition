e419959956b6ebfd829276e30d240214489f3eed
André Fontoura <45948615+AnddFontoura@users.noreply.github.com>
2022-10-14 19:52:05 -0300

    Bootstrap + js
    Adiciona bootstrap e js

---
+++ database/migrations/2022_10_14_213541_create_user_information_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_information', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('cpf', 15)->nullable(true);
+            $table->string('payment_receipt', 1000)->nullable(true);
+            $table->date('birthday')->nullable(true);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_information');
+    }
+}
