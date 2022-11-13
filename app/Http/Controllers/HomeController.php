54770b8e2e7a0fd8672ccac41777fb89f0abae34
André Fontoura <45948615+AnddFontoura@users.noreply.github.com>
2022-11-12 23:15:18 -0300

    Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Group;
+use App\Matches;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -16,18 +19,66 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
-    }
+        $userId = Auth::user()->id;
+        $userInfo = Group::where('user_id', $userId)->first();
+        $userInfoId = $userInfo->id;
+        $groups = null;
+        $groupMatches = null;
+        $octavesMatches = null;
+        $quarterMatches = null;
+        $semiMatches = null;
+        $finalMatches = null;
 
-    public function dashboard() 
-    {
-        return view('dashboard.welcome');
+        if ($userInfo) {
+            $groups = Group::where('group', $userInfo->group)
+                ->orderBy('group', 'asc')
+                ->orderBy('points', 'desc')
+                ->orderBy('goals_conceded', 'desc')
+                ->orderBy('yellow_card', 'desc')
+                ->orderBy('red_card', 'desc')
+                ->orderBy('group_position', 'asc')
+                ->get();
+
+            $groupMatches = Matches::where(function ($matches) use ($userInfoId) {
+                $matches->where('challenger_1', '=', $userInfoId)
+                    ->orWhere('challenger_2', '=', $userInfoId);
+            })
+                ->where('type', 'GROUP')
+                ->get();
+
+
+            $octavesMatches = Matches::where(function ($matches) use ($userInfoId) {
+                $matches->where('challenger_1', '=', $userInfoId)
+                    ->orWhere('challenger_2', '=', $userInfoId);
+            })
+                ->where('type', 'OCTAVES')
+                ->get();
+
+            $quarterMatches = Matches::where(function ($matches) use ($userInfoId) {
+                $matches->where('challenger_1', '=', $userInfoId)
+                    ->orWhere('challenger_2', '=', $userInfoId);
+            })
+                ->where('type', 'QUARTER')
+                ->get();
+
+            $semiMatches = Matches::where(function ($matches) use ($userInfoId) {
+                $matches->where('challenger_1', '=', $userInfoId)
+                    ->orWhere('challenger_2', '=', $userInfoId);
+            })
+                ->where('type', 'SEMI')
+                ->get();
+
+            $finalMatches = Matches::where(function ($matches) use ($userInfoId) {
+                $matches->where('challenger_1', '=', $userInfoId)
+                    ->orWhere('challenger_2', '=', $userInfoId);
+            })
+                ->where('type', 'FINAL')
+                ->get();
+
+        }
+
+        return view('home', compact('userInfo', 'groups', 'groupMatches', 'octavesMatches', 'quarterMatches', 'semiMatches', 'finalMatches'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,104 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<div class="alert alert-success" role="alert">
+    Seja bem vindo <b> {{ Auth::user()->name }} </b>
+</div>
 
-                    You are logged in!
+
+@if($groups)
+<div class="card mt-3">
+    <div class="card-header">
+        Grupo {{ $userInfo->group }}
+    </div>
+
+    <div class="card-body">
+        <table class="table table-striped w-100">
+            <thead>
+                <tr>
+                    <th style="width: 300px;"> Jogador </th>
+                    <th class="text-center" title="Pontos"> P </th>
+                    <th class="text-center" title="Jogos"> J </th>
+                    <th class="text-center" title="Vitórias"> V </th>
+                    <th class="text-center" title="Empates"> E </th>
+                    <th class="text-center" title="Derrotas"> D </th>
+                    <th class="text-center" title="Gols Sofridos"> G. S. </th>
+                    <th class="text-center" title="Gols Feitos"> G. F. </th>
+                    <th class="text-center" title="Cartões Amarelos"> C. A. </th>
+                    <th class="text-center" title="Cartões Vermelhos"> C. V. </th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach($groups as $group)
+                <tr>
+                    <td> {{ $group->userData->name }} </td>
+                    <td class="text-center"> {{ $group->points }} </td>
+                    <td class="text-center"> {{ $group->matches }} </td>
+                    <td class="text-center"> {{ $group->victories }} </td>
+                    <td class="text-center"> {{ $group->drawns }} </td>
+                    <td class="text-center"> {{ $group->defeats }} </td>
+                    <td class="text-center"> {{ $group->goals_conceded }} </td>
+                    <td class="text-center"> {{ $group->goals_scored }} </td>
+                    <td class="text-center"> {{ $group->yellow_card }} </td>
+                    <td class="text-center"> {{ $group->red_card }} </td>
+                </tr>
+
+                @endforeach
+        </table>
+    </div>
+</div>
+
+@if(count($groupMatches) > 0)
+<div class="card mt-3">
+    <div class="card-header">
+        Fase de grupos
+    </div>
+
+    <div class="card-body">
+        <div class="row">
+            @foreach($groupMatches as $match)
+            <div class="col-12 text-center">
+                <div class="row">
+                    <div class="col-md-5 col-lg-5 col-sm-12">
+                        <p>
+                            @if($userInfo->id == $match->challenger_1)
+                                <b>
+                            @endif
+                                {{ $match->challenger1->userData->name }}
+                            @if($userInfo->id == $match->challenger_1)
+                                </b>
+                            @endif
+                        </p>
+                        <h1> {{ $match->challenger_1_score }} </h1>
+                    </div>
+
+                    <div class="col-md-2 col-lg-2 col-sm-12">
+                        <h1> X </h1>
+                    </div>
+
+                    <div class="col-md-5 col-lg-5 col-sm-12">
+                        <p>
+                            @if($userInfo->id == $match->challenger_2)
+                                <b>
+                            @endif
+                                {{ $match->challenger2->userData->name }}
+                            @if($userInfo->id == $match->challenger_2)
+                                </b>
+                            @endif
+                        </p>
+                        <h1> {{ $match->challenger_2_score }} </h1>
+                    </div>
                 </div>
             </div>
+
+            <hr>
+            @endforeach
         </div>
     </div>
 </div>
+@endif
+
+@endif
 @endsection
